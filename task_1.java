c3c8e687867c639c1453e7237f9a27831426e654
unknown <tanya-mongush@mail.ru>
2024-12-07 19:23:57 +0000

    My unit test

---
+++ task_1.java
@@ -0,0 +1,8 @@
+package checkIsAdult;
+
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();  //класс
+    boolean isAdult = program.checkIsAdult(19);
+    assertEquals("Person's age is equal to or more than 18", true, isAdult);// Напиши код здесь
+}
